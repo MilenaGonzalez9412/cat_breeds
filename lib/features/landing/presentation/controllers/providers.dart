b84080e2c1f45f7aa3e404affb925b7ff2300df7
Milena Gonzalez <milecodes@gmail.com>
2025-06-28 23:11:05 -0500

    Add provider to search breeds

--- lib/features/landing/presentation/controllers/providers.dart
+++ lib/features/landing/presentation/controllers/providers.dart
@@ -7,3 +7,26 @@ final catBreedsProvider = FutureProvider<List<CatBreed>>((ref) {
   final catBreedRepository = ref.watch(catBreedsRepositoryProvider);
   return catBreedRepository.getCatBreeds();
 });
+
+final searchBreedsQueryProvider = StateProvider<String>((ref) => '');
+
+final filteredBreedsProvider = Provider<AsyncValue<List<CatBreed>>>((ref) {
+  final breedsAsync = ref.watch(catBreedsProvider);
+  final searchQuery = ref.watch(searchBreedsQueryProvider).toLowerCase();
+
+  return breedsAsync.when(
+    data: (breeds) {
+      if (searchQuery.isEmpty) {
+        return AsyncValue.data(breeds);
+      } else {
+        final filteredList =
+            breeds.where((breed) {
+              return breed.name.toLowerCase().contains(searchQuery);
+            }).toList();
+        return AsyncValue.data(filteredList);
+      }
+    },
+    error: (error, stackTrace) => AsyncValue.error(error, stackTrace),
+    loading: () => AsyncValue.loading(),
+  );
+});
